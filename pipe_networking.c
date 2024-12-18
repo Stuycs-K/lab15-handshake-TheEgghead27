1f926ad969bc9cc6052e1f01f97898009b7799a8
David Chen <dchen07@protonmail.com>
2024-12-18 13:44:19 -0600

    Fix whitespaces

--- pipe_networking.c
+++ pipe_networking.c
@@ -2,59 +2,57 @@
 //UPSTREAM = to the server / from the client
 //DOWNSTREAM = to the client / from the server
 /*=========================
-  server_setup
+	server_setup
 
-  creates the WKP and opens it, waiting for a  connection.
-  removes the WKP once a connection has been made
+	creates the WKP and opens it, waiting for a connection.
+	removes the WKP once a connection has been made
 
-  returns the file descriptor for the upstream pipe.
-  =========================*/
+	returns the file descriptor for the upstream pipe.
+	=========================*/
 int server_setup() {
-  int from_client = 0;
-  return from_client;
+	int from_client = 0;
+	return from_client;
 }
 
 /*=========================
-  server_handshake 
-  args: int * to_client
+	server_handshake
+	args: int * to_client
 
-  Performs the server side pipe 3 way handshake.
-  Sets *to_client to the file descriptor to the downstream pipe (Client's private pipe).
+	Performs the server side pipe 3 way handshake.
+	Sets *to_client to the file descriptor to the downstream pipe (Client's private pipe).
 
-  returns the file descriptor for the upstream pipe (see server setup).
-  =========================*/
+	returns the file descriptor for the upstream pipe (see server setup).
+	=========================*/
 int server_handshake(int *to_client) {
-  int from_client;
-  return from_client;
+	int from_client;
+	return from_client;
 }
 
 
 /*=========================
-  client_handshake
-  args: int * to_server
+	client_handshake
+	args: int * to_server
 
-  Performs the client side pipe 3 way handshake.
-  Sets *to_server to the file descriptor for the upstream pipe.
+	Performs the client side pipe 3 way handshake.
+	Sets *to_server to the file descriptor for the upstream pipe.
 
-  returns the file descriptor for the downstream pipe.
-  =========================*/
+	returns the file descriptor for the downstream pipe.
+	=========================*/
 int client_handshake(int *to_server) {
-  int from_server;
-  return from_server;
+	int from_server;
+	return from_server;
 }
 
 
 /*=========================
-  server_connect
-  args: int from_client
+	server_connect
+	args: int from_client
 
-  handles the subserver portion of the 3 way handshake
+	handles the subserver portion of the 3 way handshake
 
-  returns the file descriptor for the downstream pipe.
-  =========================*/
+	returns the file descriptor for the downstream pipe.
+	=========================*/
 int server_connect(int from_client) {
-  int to_client  = 0;
-  return to_client;
+	int to_client = 0;
+	return to_client;
 }
-
-
